6e16c5ae8eafe85af6cd599c2ebc349365d285b3
VAntipenkoDev <157708143+VAntipenkoDev@users.noreply.github.com>
2024-07-06 11:41:34 +0300

    Add date validation

--- src/components/forms/signUp/validation.js
+++ src/components/forms/signUp/validation.js
@@ -4,6 +4,9 @@ const SignUpSchema = Yup.object().shape({
   name: Yup.string()
     .required('Required')
     .min(3, 'Must be at least 3 characters'),
+  day: Yup.string().required('Required'),
+  month: Yup.string().required('Required'),
+  year: Yup.string().required('Required'),
 });
 
 export default SignUpSchema;
