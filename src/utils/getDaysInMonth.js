86a4e834682a633491c0a48d9924204d25a434dd
VAntipenkoDev <157708143+VAntipenkoDev@users.noreply.github.com>
2024-07-09 15:30:34 +0300

    Fix days count

--- src/utils/getDaysInMonth.js
+++ src/utils/getDaysInMonth.js
@@ -2,7 +2,7 @@ export const getDaysInMonth = ({ month, year }) => {
   let days = {};
   const dayCount = new Date(year, month, 0).getDate();
 
-  for (let i = 0; i <= dayCount; i++) {
+  for (let i = 1; i <= dayCount; i++) {
     days = {
       ...days,
       [i]: i,
