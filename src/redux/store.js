c408de1bb44676b2a17af770ffda77ea8100fa86
VAntipenkoDev <157708143+VAntipenkoDev@users.noreply.github.com>
2024-07-09 15:32:17 +0300

    Add reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import signUpReducer from './reducers/signUpReducer';
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    signUp: signUpReducer,
+  },
 });
