df4fd950e620fb070bbcde7c4cb63030fc207dea
VAntipenkoDev <157708143+VAntipenkoDev@users.noreply.github.com>
2024-07-16 06:54:08 +0300

    Add new icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,6 +1,29 @@
 import MassageSVG from './Message.svg';
 import UnlockSVG from './Unlock.svg';
 import ProfileSVG from './Profile.svg';
+import ActivitySVG from './Activity.svg';
+import ChartSVG from './Chart.svg';
+import GameSVG from './Game.svg';
+import HomeSVG from './Home.svg';
+import LogoutSVG from './Logout.svg';
 import ArrowDownSVG from './ArrowDown.svg';
+import PetHouseSVG from './PetHouse.svg';
+import RabbitSVG from './Rabbit.svg';
+import RouletteSVG from './Roulette.svg';
+import SettingSVG from './Setting.svg';
 
-export { MassageSVG, UnlockSVG, ProfileSVG, ArrowDownSVG };
+export {
+  MassageSVG,
+  UnlockSVG,
+  ProfileSVG,
+  ArrowDownSVG,
+  ActivitySVG,
+  ChartSVG,
+  GameSVG,
+  HomeSVG,
+  LogoutSVG,
+  PetHouseSVG,
+  RabbitSVG,
+  SettingSVG,
+  RouletteSVG,
+};
