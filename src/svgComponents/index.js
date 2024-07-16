d38522dab2e76b8a3443daa4fe57a8767fd4056c
VAntipenkoDev <157708143+VAntipenkoDev@users.noreply.github.com>
2024-07-16 06:54:42 +0300

    import/export

---
+++ src/svgComponents/index.js
@@ -0,0 +1,19 @@
+import SVGActivity from './SVGActivity';
+import SVGChart from './SVGChart';
+import SVGRoulette from './SVGRoulette';
+import SVGUser from './SVGUser';
+import SVGGame from './SVGGame';
+import SVGRabbit from './SVGRabbit';
+import SVGHome from './SVGHome';
+import SVGPetHouse from './SVGPetHouse';
+
+export {
+  SVGActivity,
+  SVGChart,
+  SVGRoulette,
+  SVGUser,
+  SVGGame,
+  SVGRabbit,
+  SVGHome,
+  SVGPetHouse,
+};
